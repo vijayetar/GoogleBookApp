72bff0884b8ead4cd147531a6f925297531b9c43
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-21 10:18:36 -0800

    connected public and views to proof of life

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express= require('express');
+require('dotenv').config();
+const PORT = process.env.PORT||3001;
+require('ejs');
+const superagent = require('superagent');
+
+const app = express();
+
+//connecting public
+app.use(express.static('./public'));
+
+//set up the view engine
+app.set('view engine', 'ejs'); 
+
+//bodyParser
+app.use(express.urlencoded({extended:true}));
+
+
+//routes
+app.get('/', getHomePage);
+// app.get('/searches/new', displaySearch);
+// app.post('/searches/new', collectBookSearchData);
+
+//functions
+function getHomePage(request,response){
+  response.status(200).render('./pages/index');
+}
+
+
+
+app.listen(PORT, ()=> (console.log(`Ally,Vij and Cait are chatting on ${PORT}`)));
