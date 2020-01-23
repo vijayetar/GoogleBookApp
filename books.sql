80743cbce8e4f7f15c6e24ac66193111bebf8f9a
Allyson Reyes <areyes986@gmail.com>
2020-01-23 11:35:27 -0800

    push

--- books.sql
+++ books.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS book_table;
 
 CREATE TABLE IF NOT EXISTS book_table (
     id SERIAL PRIMARY KEY, 
-    author VARCHAR(255),
+    authors VARCHAR(255),
     title VARCHAR(255),
-    isbn VARCHAR(30),
     image_url VARCHAR(255),
-    description VARCHAR(255)
+    description VARCHAR(255),
+    bookshelf VARCHAR(255)
 );
 
-INSERT INTO book_table (author, title, isbn, image_url, description) VALUES ('Vij', 'Vij', 'Vij', 'vij', 'vijjjjj');
+INSERT INTO book_table (authors, title, image_url, description, bookshelf) VALUES ('Vij', 'Vij', 'Vij', 'vij', 'vijjjjj');
